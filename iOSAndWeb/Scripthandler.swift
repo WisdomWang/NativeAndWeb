c7988cf5b72ce122e1f99bc6ff3270edd3b178c4
WisdomWang <294814110@qq.com>
2019-02-26 18:00:41 +0800

    first commit

---
+++ iOSAndWeb/Scripthandler.swift
@@ -0,0 +1,13 @@
+//
+//  Scripthandler.swift
+//  iOSAndWeb
+//
+//  Created by Cary on 2019/2/26.
+//  Copyright © 2019 Cary. All rights reserved.
+//
+
+import UIKit
+
+class Scripthandler: NSObject {
+
+}
